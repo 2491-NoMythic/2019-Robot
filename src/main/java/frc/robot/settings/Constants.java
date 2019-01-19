8a703ae74f6c529b8fd9546c70f3d4da830e2f32
gballen <35871516+gballen@users.noreply.github.com>
2019-01-19 14:27:53 -0600

    Constants-class, Drivetrain-subsystem
    Added talon channels, new stuff for drivetrain.

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -1,6 +1,8 @@
 
 package frc.robot.settings;
 
+//If the value is 2491, it is a placeholder for the actual channel and will need to be changed to work on the robot.
+
 public class Constants {
 
   //Drive
@@ -8,4 +10,18 @@ public class Constants {
   public static final int driveTalonRightChannel = 2491;
   public static final int driveVictorLeftChannel = 2491;
   public static final int driveVictorRightChannel = 2491;
+
+  //Climber
+  public static final int climberRodTalonLeftChannel = 2491;
+  public static final int climberRodTalonRightChannel = 2491;
+  public static final int climberRollerTalonChannel = 2491;
+
+  //Box
+  public static final int hatchClawSolenoidLeftChannel = 2491;
+  public static final int hatchClawSolenoidRightChannel = 2491;
+  public static final int rollerTalonChannel = 2491;
+
+  //TalonSRX
+  public static final int kTimeoutMs = 2491;
+  public static final int kVelocitySlotId = 2491;
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,26 +8,23 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import frc.robot.settings.Constants;
 import frc.robot.settings.Variables;
-import sun.security.jca.GetInstance;
 
 
 
-
-/**
- * Add your docs here.
- */
 public class Drivetrain extends PIDSubsystem {
   private TalonSRX rightMaster, leftMaster;
-  private VictorSPX rightFollower, leftFollower;
+  private VictorSPX rightSlave, leftSlave;
   private NetworkTable limelight;
   private NetworkTableEntry tx, ty, ta, tv;
   private double currentPIDOutput;
@@ -42,24 +39,46 @@ public class Drivetrain extends PIDSubsystem {
   }
 
   private Drivetrain() {
-    super("Drive", Variables.ProportionalRotate, Variables.integralRotate, Variables.derivativeRotate);
+    super("Drive", Variables.proportionalRotate, Variables.integralRotate, Variables.derivativeRotate);
 
     try {
       leftMaster = new TalonSRX(Constants.driveTalonLeftChannel);
       rightMaster = new TalonSRX(Constants.driveTalonRightChannel);
-      leftFollower = new VictorSPX(Constants.driveVictorLeftChannel);
-      rightFollower = new VictorSPX(Constants.driveVictorRightChannel);
+      leftSlave = new VictorSPX(Constants.driveVictorLeftChannel);
+      rightSlave = new VictorSPX(Constants.driveVictorRightChannel);
     }
     catch (Exception e) {
       DriverStation.reportError("TalonSRX instantiation failure! Check CAN Bus, TalonSRX Decive ID's, and TalonSRX power", Variables.debugMode);
 
       if (Variables.debugMode) {System.out.println(e);}
     }
+
+    //Binds Victor slaves to Talon masters
+    leftSlave.follow(leftMaster);
+    rightSlave.follow(rightMaster);
+
+    //Configures Talon feedback sensors
+    leftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kVelocitySlotId, Constants.kTimeoutMs);
+    rightMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kVelocitySlotId, Constants.kTimeoutMs);
+
+    //Corrects sensor direction to match throttle direction
+    leftMaster.setSensorPhase(true);
+    rightMaster.setSensorPhase(true);
   }
 
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  protected void initDefaultCommand() {
+  }
+
+  @Override
+  //Add in gyro stuff later
+  public double returnPIDInput() {
+    return 0;
+  }
+
+  @Override
+  //Add gyro stuff later
+  protected void usePIDOutput(double output) {
+    
   }
 }
